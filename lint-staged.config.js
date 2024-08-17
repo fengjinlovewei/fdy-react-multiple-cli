8686ba1d3bce54e30b79aa90bff4273eaf31bc1a
fengjin02 <fengjin02@58.com>
2024-08-18 07:46:55 +0800

    test: jest不能放在lint-staged里,因为每次提交都需要全局检查,而不是只检查变动的文件

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   // git add . 后， git commit 之前执行的命令
   // lint-staged 作用 对已经通过 git add 加入到 提交区 stage 的文件进行扫描即可。
   // --passWithNoTests 表示没有测试用例返回0，而不是1
-  '*.{js,jsx,ts,tsx}': ['jest --passWithNoTests', 'prettier --write', 'eslint --fix'],
+  '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix'],
 };
